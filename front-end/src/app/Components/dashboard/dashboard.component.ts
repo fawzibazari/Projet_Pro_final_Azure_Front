2a050ba8bc2165e35de9503309a056eef8290cf1
SlimaneBenamar <49103069+SlimaneBenamar@users.noreply.github.com>
2023-04-27 16:53:36 +0200

    working on display content

--- front-end/src/app/Components/dashboard/dashboard.component.ts
+++ front-end/src/app/Components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +7,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DashboardComponent implements OnInit {
 
+  @ViewChild("fileUpload", {static: false}) fileUpload!: ElementRef;
+
   DJANGO_SERVER: string = "http://127.0.0.1:8000";
   imageSrc: string = "";
   fileName: any;
+  fileType: any;
+  base64Image: any;
+  loaded: boolean = false;
 
 
   constructor( 
@@ -32,21 +37,27 @@ export class DashboardComponent implements OnInit {
     reader.onload = this._handleReaderLoaded.bind(this);
     reader.readAsDataURL(file);
     this.fileName = file.name;
+    this.loaded = true;
   }
 
   _handleReaderLoaded(e: any) {
       let reader = e.target;
       this.imageSrc = reader.result;
 
-      const base64Image =this.imageSrc.split(",")[1];
-      const fileType = this.imageSrc.split("/")[1].split(";")[0];
+      this.base64Image =this.imageSrc.split(",")[1];
+      this.fileType = this.imageSrc.split("/")[1].split(";")[0];
       //console.log("file type --------->"+ fileType);
       //console.log(base64Image);
-      const url = this.DJANGO_SERVER + "/api/create";
+  }
 
-      this.httpClient.post(url, {filename: this.fileName.split(".")[0], extension: fileType,img: base64Image}).subscribe( 
+  handleSubmit(e: any){
+    const url = this.DJANGO_SERVER + "/api/create";
+
+      this.httpClient.post(url, {filename: this.fileName.split(".")[0], extension: this.fileType,img: this.base64Image}).subscribe( 
         res => {
           console.log(res);
+          this.loaded = false;
+          this.fileUpload.nativeElement.value = "";
         },
         err => {
           console.log(err);
